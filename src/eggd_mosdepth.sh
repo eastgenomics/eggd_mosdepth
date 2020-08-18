ecaeab7731f2729ac151c819c376f156afc1b9fc
jethror1 <rainford1995@gmail.com>
2020-08-18 18:17:49 +0100

    fix running in docker container

--- src/eggd_mosdepth.sh
+++ src/eggd_mosdepth.sh
@@ -23,65 +23,50 @@ main() {
       optional_arguments="${optional_arguments} ${bed_arg}"
     fi
 
-    if [[ $optional_arguments =~ "--quantize" ]]; then
-        # if --quantize option given
-        if [[ $quantize_labels ]]; then
-          # optional labels passed
-          echo "Using user defined labels"
-          IFS=',' read -r -a array <<< "$quantize_labels"
-          for i in "${!array[@]}"
-          # loop over label array, pass label no. and label
-          do
-              export MOSDEPTH_Q$i="${array[$i]}"
-          done
-    
-        else
-        # no labels given, use default
-        echo "Using default 4 labels:"
-        echo "MOSDEPTH_Q0=NO_COVERAGE"
-        echo "MOSDEPTH_Q1=LOW_COVERAGE"
-        echo "MOSDEPTH_Q2=CALLABLE"
-        echo "MOSDEPTH_Q3=HIGH_COVERAGE"
-        
-        export MOSDEPTH_Q0=NO_COVERAGE
-        export MOSDEPTH_Q1=LOW_COVERAGE
-        export MOSDEPTH_Q2=CALLABLE
-        export MOSDEPTH_Q3=HIGH_COVERAGE
-        fi
-    fi
-
     # check if set, if not set to empty string
     if [[ -z $optional_arguments ]]; then
       optional_arguments=""
     fi
     
-    echo "Using following optional arguments:"
-    echo $optional_arguments
-    echo $quantize_labels
-
     # add dnanexus user to docker group
     sudo usermod -a -G docker dnanexus
     newgrp docker
-
     sudo systemctl start docker
 
     # load local container & get id
     sudo docker load --input mosdepth_container.tar
     mosdepth_id=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^quay.io" | cut -d' ' -f2) 
 
-    # command to run mosdepth
-    cmd="mosdepth $optional_arguments $bam_prefix ../../input/*bam"
-    echo $cmd
 
-    # run container with ID and mosdepth cmd
-    sudo docker run -v "/home/dnanexus/input:/input" -v "/home/dnanexus/out/:/out" -w "/out/mosdepth_output" $mosdepth_id /bin/bash -c $cmd
-
-    echo "output:"   
-    ls out/mosdepth_output
+    if [[ $optional_arguments =~ "--quantize" ]]; then
+        # if --quantize option given
+        if [[ $quantize_labels ]]; then
+          # optional labels passed
+          # loop over label array, export label no. and label, then run mosdepth from docker container
+          sudo docker run -v `pwd`:/data -w "/data/out/mosdepth_output" $mosdepth_id /bin/bash -c \
+          "IFS=',' read -r -a array <<< '$quantize_labels';
+          for i in "${!array[@]}";
+          do export MOSDEPTH_Q$i="${array[$i]}" && echo MOSDEPTH_Q$i="${array[$i]}";
+          done;
+          mosdepth $optional_arguments $bam_prefix '/data/input/$bam_prefix.bam'"  
+        else
+        # no labels given, use default
+        sudo docker run -v `pwd`:/data -w "/data/out/mosdepth_output" $mosdepth_id /bin/bash -c \
+        "export MOSDEPTH_Q0=NO_COVERAGE; 
+          export MOSDEPTH_Q1=LOW_COVERAGE;
+          export MOSDEPTH_Q2=CALLABLE;
+          export MOSDEPTH_Q3=HIGH_COVERAGE;
+          mosdepth $optional_arguments $bam_prefix '/data/input/$bam_prefix.bam'"
+        fi
+    else
+    # not using quantize option, run mosdepth normally with container
+    sudo docker run -v `pwd`:/data -w "/data/out/mosdepth_output" $mosdepth_id mosdepth $optional_arguments $bam_prefix "/data/input/$bam_prefix.bam"
+    fi
 
     echo "app finished, uploading files"
+    
     # upload output files
     dx-upload-all-outputs
-    cp this that
+
     echo "uploaded"
 }
