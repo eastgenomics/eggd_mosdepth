cbe66e4ecd547317aae6433c0f79bc9aec62ac0d
jethror1 <rainford1995@gmail.com>
2020-08-19 14:40:51 +0100

    Fixed to use local docker container

--- src/eggd_mosdepth.sh
+++ src/eggd_mosdepth.sh
@@ -13,22 +13,20 @@ main() {
     dx-download-all-inputs
     find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/input
 
-    if [[ $bed ]]; then
-      # if bed file is being used
-
-      # DNAnexus doesn't seem to handle passing files through optional string well
-      # get full path of bed, then add the full bed path after --by to optional args
-      bed_path=$(realpath ~/input/*.bed)
-      bed_arg="--by $bed_path"
-      optional_arguments="${optional_arguments} ${bed_arg}"
-    fi
-
     # check if set, if not set to empty string
     if [[ -z $optional_arguments ]]; then
       optional_arguments=""
     fi
+
+    if [[ $bed ]]; then
+      # if bed file is being used
+      # specify path of bed in container, then add the full bed path after --by to optional args
+      bed_path="/data/input/$bed_prefix.bed"
+      bed_arg="--by $bed_path"
+      optional_arguments="${optional_arguments} ${bed_arg}"
+    fi
     
-    # add dnanexus user to docker group
+    # add dnanexus user to docker group & start docker daemon
     sudo usermod -a -G docker dnanexus
     newgrp docker
     sudo systemctl start docker
@@ -42,21 +40,36 @@ main() {
         # if --quantize option given
         if [[ $quantize_labels ]]; then
           # optional labels passed
-          # loop over label array, export label no. and label, then run mosdepth from docker container
-          sudo docker run -v `pwd`:/data -w "/data/out/mosdepth_output" $mosdepth_id /bin/bash -c \
-          "IFS=',' read -r -a array <<< '$quantize_labels';
-          for i in "${!array[@]}";
-          do export MOSDEPTH_Q$i="${array[$i]}" && echo MOSDEPTH_Q$i="${array[$i]}";
-          done;
-          mosdepth $optional_arguments $bam_prefix '/data/input/$bam_prefix.bam'"  
+          echo "using specified labels"
+          echo $quantize_labels
+
+          # build string of labels from array in format for mosdepth
+          IFS=", " read -r -a array <<< $quantize_labels
+          labels=""
+          for i in "${!array[@]}"; do 
+            labels+=MOSDEPTH_Q$i="${array[$i]}" && labels+=" ";
+          done
+
+          # set bam path variable to pass
+          bam_file="/data/input/$bam_prefix.bam"
+
+          # pass env variables to container, loop to export labels to container env varibales
+          # then run mosdepth
+          sudo docker run -v `pwd`:/data \
+          --env labels="$labels" --env optional_arguments="$optional_arguments" \
+          --env bam_prefix="$bam_prefix" --env bam_file="$bam_file" \
+          -w "/data/out/mosdepth_output" \
+          $mosdepth_id /bin/bash -c \
+          'for label in $labels; do export $label; done; mosdepth $optional_arguments $bam_prefix $bam_file'  
         else
+
         # no labels given, use default
         sudo docker run -v `pwd`:/data -w "/data/out/mosdepth_output" $mosdepth_id /bin/bash -c \
         "export MOSDEPTH_Q0=NO_COVERAGE; 
-          export MOSDEPTH_Q1=LOW_COVERAGE;
-          export MOSDEPTH_Q2=CALLABLE;
-          export MOSDEPTH_Q3=HIGH_COVERAGE;
-          mosdepth $optional_arguments $bam_prefix '/data/input/$bam_prefix.bam'"
+         export MOSDEPTH_Q1=LOW_COVERAGE;
+         export MOSDEPTH_Q2=CALLABLE;
+         export MOSDEPTH_Q3=HIGH_COVERAGE;
+         mosdepth $optional_arguments $bam_prefix '/data/input/$bam_prefix.bam'"
         fi
     else
     # not using quantize option, run mosdepth normally with container
